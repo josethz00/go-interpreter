e1405bff84ff6b30a3f9807f46cff8dd2b2adc00
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-07 21:03:10 -0300

    feat: lexer nextToken fn

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go-interpreter/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (it points to current char)
@@ -10,4 +12,19 @@ type Lexer struct {
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	// [...]
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier() // if is a letter at start, delegate it to a identifier function
+			return tok
+		} else { // otherwise is illegal
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
 }
