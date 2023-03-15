aeede51ccf9674d20f9e31f50059d0b947604395
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-15 10:07:21 -0300

    test(lexer): add boolean values

--- lexer/lexer-test.go
+++ lexer/lexer-test.go
@@ -8,15 +8,17 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
-
 	let add = fn(x, y) {
-		x + y;
+	x + y;
 	};
-
 	let result = add(five, ten);
 	!-/*5;
 	5 < 10 > 5;
-	`
+	if (5 < 10) {
+	return true;
+	} else {
+	return false;
+	}`
 
 	tests := []struct {
 		expectedType    token.TokenType
